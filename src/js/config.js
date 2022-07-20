9a2d9cd6b4e4720fed08574f4971b126733ec76b
TamaraCuric <tamara.curic01@gmail.com>
2022-07-20 18:23:32 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9d7829be-3683-467d-aa08-ff8d2393b45e';
+export const MODAL_CLOSE_SEC = 2.5;
